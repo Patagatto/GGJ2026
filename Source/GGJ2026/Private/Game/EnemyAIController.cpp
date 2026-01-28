264205cfb0cf659d3f1d3865c2b699490ad28d36
Umberto <u.gentile@itsrossellini.it>
2026-01-28 13:04:24 +0100

    added enemy controller, spawner and class

---
+++ Source/GGJ2026/Private/Game/EnemyAIController.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/EnemyAIController.h"
+
+#include "Kismet/GameplayStatics.h"
+
+void AEnemyAIController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AEnemyAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	
+	if (AIBehaviorTree)
+	{
+		RunBehaviorTree(AIBehaviorTree);
+	}
+}
---
+++ Source/GGJ2026/Public/Game/EnemyAIController.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "EnemyAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGJ2026_API AEnemyAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void BeginPlay() override;
+	
+	virtual void OnPossess(APawn* InPawn) override;
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+	UBehaviorTree* AIBehaviorTree;
+};
