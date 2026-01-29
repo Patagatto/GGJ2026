485d550767255d5bb6845ee82c11292a5cc1216f
Umberto <umberto.gen28@gmail.com>
2026-01-29 12:32:15 +0100

    added attack manager with tokens

---
+++ Source/GGJ2026/Public/AI/EnemyManager.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "EnemyManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGJ2026_API UEnemyManager : public UWorldSubsystem
+{
+	GENERATED_BODY()
+		
+protected:
+	UPROPERTY(EditAnywhere)
+	int32 MaxToken = 2;
+	
+	UPROPERTY()
+	TSet<AActor*> ActiveTokenHolders;
+	
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	
+	// Attack Token Handling
+	UFUNCTION(BlueprintCallable)
+	bool HasToken(AActor* EnemyActor) const;
+	
+	UFUNCTION(BlueprintCallable)
+	bool RequestAttack(AActor* EnemyActor);
+	
+	UFUNCTION(BlueprintCallable)
+	void ReleaseToken(const AActor* EnemyActor);
+};
